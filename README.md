514e50fc8a5a4a1864c003b55de8a18247ffc9dd
Salem Elrahal <salem.elrahal@gmail.com>
2016-06-01 13:51:58 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Simple Drools Hadoop Example
+Build with 
+
+```
+mvn clean package
+```
+
+This will create a jar, `target/drools-mapreduce-hadoop-example-1.0.0-SNAPSHOT.jar`, that can be uploaded and run as a Hadoop MapReduce job.
---
+++ src/main/java/org/drools/mapreduce/hadoop/HadoopDroolsMapper.java
@@ -0,0 +1,44 @@
+package org.drools.mapreduce.hadoop;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.drools.core.ClassObjectFilter;
+import org.drools.mapreduce.hadoop.model.Entry;
+import org.drools.mapreduce.hadoop.model.Transaction;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
+
+public class HadoopDroolsMapper extends Mapper<LongWritable, Text, Text, DoubleWritable>{
+	private static KieContainer kieContainer = KieServices.Factory.get().newKieClasspathContainer();
+
+    @Override
+    public void map(LongWritable key, Text value, Context output) throws IOException, InterruptedException {
+    	KieSession kieSession = kieContainer.getKieBase().newKieSession();
+    	
+        //Input is a transaction
+        Transaction transaction = new Transaction(value.toString());
+        kieSession.insert(transaction);
+        
+        
+        //Fire mapper-rules
+        kieSession.getAgenda().getAgendaGroup("mapper-rules").setFocus();
+        kieSession.fireAllRules();
+        Collection<FactHandle> entries = kieSession.getFactHandles(new ClassObjectFilter(Entry.class));
+        
+        //Get all entries created as outputs
+        for (FactHandle handle : entries) {
+        	Entry entry = (Entry) kieSession.getObject(handle);
+        	output.write(new Text(entry.key), new DoubleWritable(entry.value));
+        }
+        
+        kieSession.dispose();
+    }
+
+}
